b1f3d0d5cc1a1899988cb098f529f26cc143750e
Matthew <e1155311@u.nus.edu>
2025-03-03 21:48:02 +0800

    Implement SLAP to all .java files

---
+++ src/main/java/seedu/duke/menu/HelpPage.java
@@ -0,0 +1,20 @@
+package seedu.duke.menu;
+import seedu.duke.commands.Commands;
+import seedu.duke.messages.Messages;
+
+public class HelpPage {
+
+    public void displayCommandList() {
+        Messages messages = new Messages();
+        messages.setDivider();
+        String commandList = Commands.HELP + ": " + Commands.getCommandDescription(Commands.HELP) 
+                            + System.lineSeparator()
+                            + Commands.CATEGORIES + ": " + Commands.getCommandDescription(Commands.CATEGORIES)
+                            + System.lineSeparator()
+                            + Commands.EXIT_APP + ": " + Commands.getCommandDescription(Commands.EXIT_APP) 
+                            + System.lineSeparator();
+
+        System.out.println("COMMANDS: " + System.lineSeparator());
+        System.out.println(commandList);
+    }
+}
