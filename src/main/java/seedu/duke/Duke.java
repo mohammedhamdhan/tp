82ff13b998ec8a42777e28bc0dcdd5a84e75ff87
Matthew <e1155311@u.nus.edu>
2025-03-05 15:23:17 +0800

    Refactor code

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,5 +1,6 @@
 package seedu.duke;
 
+import java.io.IOException;
 import java.util.Scanner;
 import seedu.duke.ui.UI;
 import seedu.duke.messages.Messages;
@@ -7,23 +8,31 @@ import seedu.duke.menu.HelpPage;
 import seedu.duke.storage.DataStorage;
 
 public class Duke {
-    private final String storageFilePath;
+    private DataStorage storage;
+    private UI ui;
+    private Scanner scanner;
+    private Messages messages;
+    private HelpPage helpPage;
 
     public Duke(String fileName) {
-        this.storageFilePath = fileName;  
-        DataStorage.ensureFileExists();
+        storage = new DataStorage(fileName);
+        scanner = new Scanner(System.in);
+        messages = new Messages();
+        helpPage = new HelpPage();
+        ui = new UI(scanner, messages, helpPage, fileName);
+        try {
+            storage.createFileIfAbsent();
+            storage.load();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
-        new Duke(DataStorage.DATA_FILE).run();
+        new Duke("./data/data.txt").run();
     }
 
     public void run() {
-        Scanner scanner = new Scanner(System.in);
-        Messages messages = new Messages();
-        HelpPage helpPage = new HelpPage();
-        UI ui = new UI(scanner, messages, helpPage, storageFilePath);
-
         messages.displayWelcomeMessage();
         helpPage.displayCommandList();
         messages.setDivider();
--- src/main/java/seedu/duke/storage/DataStorage.java
+++ src/main/java/seedu/duke/storage/DataStorage.java
@@ -1,29 +1,31 @@
 package seedu.duke.storage;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import seedu.duke.messages.Messages;
 
 public class DataStorage {
-    public static final String DATA_FILE = "C:\\Users\\user\\tp-1\\src\\main\\java\\seedu\\duke\\storage\\data.txt";
+    private String fileName;
 
-    public static void ensureFileExists() {
-        File file = new File(DATA_FILE);
+    public DataStorage(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void load() throws FileNotFoundException {
+        File file = new File(fileName);
         if (!file.exists()) {
-            throw new IllegalStateException(Messages.errorMessageTag()
-                    + Messages.missingTaskFileErrorMessage()
-                    + file.getAbsolutePath());
+            throw new FileNotFoundException("File does not exist: " + file.getAbsolutePath());
         }
     }
 
-
-    public static List<String> loadData() {
+    public static List<String> loadData(String filePath) {
         List<String> data = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(DATA_FILE))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 data.add(line);
@@ -36,13 +38,24 @@ public class DataStorage {
 
     public static void displayStoredData(String filePath) {
         Messages.loadDataMessage(filePath);
-        List<String> data = loadData();
+        List<String> data = loadData(filePath);
         if (data.isEmpty()) {
             Messages.emptyDataFileMessage();
         } else {
             for (String line : data) {
                 System.out.println(line);
             }
+        }
+    }
+
+    public void createFileIfAbsent() throws IOException {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            File parentDir = file.getParentFile();
+            if (parentDir != null && !parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+            file.createNewFile();
         }
     }
 }
