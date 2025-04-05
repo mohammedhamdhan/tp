0c5288449f0d0fd3edfc3a82efe1c3c702ee4be1
NandhithaShree <142162325+NandhithaShree@users.noreply.github.com>
2025-04-05 16:13:41 +0800

    Removed dollar sign in balance to accurately represent different currencies

--- src/main/java/seedu/duke/commands/ExpenseCommand.java
+++ src/main/java/seedu/duke/commands/ExpenseCommand.java
@@ -379,7 +379,7 @@ public class ExpenseCommand {
         System.out.println("Balance Overview");
         System.out.println("----------------");
         System.out.println("Total number of unsettled expenses: " + budgetManager.getUnsettledExpenseCount());
-        System.out.println("Total amount owed: $" + String.format("%.2f", totalBalance));
+        System.out.println("Total amount owed: " + String.format("%.2f", totalBalance));
     }
     //@@author
 
